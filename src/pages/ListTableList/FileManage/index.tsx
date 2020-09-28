d0da8fa73d9df48529a3def0bd70c15cfd839778
cwhu <cunwei.hu@emapgo.com.cn>
2020-09-28 11:04:46 +0800

    update

--- src/pages/ListTableList/FileManage/index.tsx
+++ src/pages/ListTableList/FileManage/index.tsx
@@ -2,7 +2,6 @@ import { PageContainer } from '@ant-design/pro-layout';
 import React, { useState, useEffect } from 'react';
 import { Spin } from 'antd';
 import styles from './index.less';
-
 export default () => {
   const [loading, setLoading] = useState<boolean>(true);
   useEffect(() => {
@@ -12,7 +11,12 @@ export default () => {
   }, []);
   return (
     <PageContainer content="这是一个新页面，从这里进行开发！" className={styles.main}>
-      <div style={{ paddingTop: 100, textAlign: 'center' }}>
+      <div
+        style={{
+          paddingTop: 100,
+          textAlign: 'center',
+        }}
+      >
         <Spin spinning={loading} size="large" />
       </div>
     </PageContainer>
